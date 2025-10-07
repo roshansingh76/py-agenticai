58ad5de70f62617015724cd3d964a6d0e57ab0bd
roshan singh <roshansingh9450@gmail.com>
2025-10-07 23:19:18 +0530

    remove route file

--- src/main.py
+++ src/main.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI
 from src.routes.main_router import router as main_router
 
 app = FastAPI(
-    root_path='agenticai',
     title="Py-Agentic AI",
     version="1.0.0",
     description="FastAPI app with modular routing"
