dc75b9f2bb4f84d92b229384026b43cb14a0fac0
roshan singh <roshansingh9450@gmail.com>
2025-10-09 23:06:28 +0530

    pipeline testing

--- src/routes/user.py
+++ src/routes/user.py
@@ -4,7 +4,7 @@ router = APIRouter()
 
 @router.get("/me")
 async def read_user_me():
-    return {"user_id": "the current user asdasdas"}
+    return {"user_id": "the current user test pipeline"}
 @router.get("/login")
 async def read_user_login():
     return {"user_id": "the current user login  "}
