f971c1de659733aae9a508fbdbee586428aa5062
roshan singh <roshansingh9450@gmail.com>
2025-10-14 00:05:11 +0530

    Update file

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from src.routes.main_router import router as main_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     root_path='/agenticai',
@@ -7,6 +8,20 @@ app = FastAPI(
     version="1.0.0",
     description="FastAPI app with modular routing"
 )
+
+origins = [
+    "http://localhost:3000",  # React frontend
+    # you can add other allowed origins here
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,       # or ["*"] to allow all origins (not recommended for production)
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 # Include all routers
 app.include_router(main_router, prefix="/api/v1")
--- src/routes/main_router.py
+++ src/routes/main_router.py
@@ -1,8 +1,7 @@
 from fastapi import APIRouter
 from src.routes.user import router as user_router
-from src.routes.chat import router as chat_router   
+
 
 router = APIRouter()
 
 router.include_router(user_router, prefix="/user", tags=["User"])
-router.include_router(chat_router, prefix="/chat", tags=["Chat"])
